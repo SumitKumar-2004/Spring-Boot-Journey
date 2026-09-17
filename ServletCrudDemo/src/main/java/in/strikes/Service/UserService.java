bf40e173a7e8ca8296b9f41978b37badef08e2d4
Sumit <skdudy546@gmail.com>
2026-09-17 09:54:30 +0530

    Add Servlet CRUD Demo

---
+++ ServletCrudDemo/src/main/java/in/strikes/Service/UserService.java
@@ -0,0 +1,35 @@
+package in.strikes.Service;
+
+import in.strikes.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserService {
+    private Map<Integer, User> userDB;
+
+    public UserService() {
+        userDB = new HashMap<>();
+    }
+
+    public User createUser(User userReq) {
+        userDB.put(userReq.getId(), userReq);
+        return userReq;
+    }
+
+    public List<User> getAllUsers() {
+        List<User> userResp = new ArrayList<>();
+        for(User user : userDB.values()){
+            userResp.add(user);
+        }
+        return userResp;
+    }
+
+    public User getUserById(Integer id) {
+        return userDB.getOrDefault(id, null);
+    }
+
+
+}
